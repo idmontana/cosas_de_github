70c309271882c78a32a4445ccdf65f6165d82c89
idmontana <64493635+idmontana@users.noreply.github.com>
2020-05-30 10:31:30 -0500

    Create funciones.h

---
+++ hw09/funciones.h
@@ -0,0 +1,14 @@
+
+double alfa(double t,double vo){
+    double vi= vo - 9.8*t; 
+    return vi;
+}
+
+double RungeKutta4(double vo, double y, double t, double dt){
+    double k1 = alfa(t,vo)*dt;
+    double k2 = alfa(t+dt/2,vo)*dt;
+    double k3 = alfa(t+dt/2,vo)*dt;
+    double k4 = alfa(t+dt,vo)*dt;
+    double ynew = y + (k1 + 2*k2 + 2*k3 + k4)/6;
+    return ynew;
+}
