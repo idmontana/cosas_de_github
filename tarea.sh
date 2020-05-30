2e18db0a5ec3031e49b17419ba71dfc1a258a83a
idmontana <64493635+idmontana@users.noreply.github.com>
2020-05-30 10:34:57 -0500

    Delete tarea.sh

--- tarea.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Compila el código main, ejecuta el archivo ejecutable a.out
-# y destina los resultados a datos.txt
-g++ main.cpp && ./a.out > datos.txt
-#Con los datos de datos.txt, grafica en python la función.
-python grafica.py
